8bb5c3b9cd3451f1e35ce98b300b5ce0cecda2a6
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-07-06 11:32:44 +1200

    Start code

--- start_code.py
+++ start_code.py
@@ -1,8 +1,17 @@
 def checkio(data):
 
     #replace this for solution
-    return None
+    return True or False
+
+#Some hints
+#Just check all conditions
+
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    pass
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"

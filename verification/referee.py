3df5959503d997027e05987d3cfdfbd520d46351
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-11-23 11:49:22 +1200

    Cover for boolean

--- verification/referee.py
+++ verification/referee.py
@@ -4,6 +4,14 @@ from checkio.referees.io import CheckiOReferee
 
 from tests import TESTS
 
+cover = """
+def cover(func, in_data):
+    return bool(func(in_data))
+"""
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(tests=TESTS).on_ready)
+    CheckiOReferee(tests=TESTS,
+                   cover_code={
+                       "python-27": cover,
+                       "python-3": cover,
+                   }).on_ready)

c3e7f5c59957f2cfd3da247fa8eb8c078e47f741
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-12-22 16:52:38 +1200

    add test

--- verification/tests.py
+++ verification/tests.py
@@ -7,82 +7,86 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input":"ULFFunH8ni",
-            "answer":True
+            "input": "ULFFunH8ni",
+            "answer": True
         },
         {
-            "input":"aaaaaaaaaaaaaaaaaaaaa",
-            "answer":False
+            "input": "aaaaaaaaaaaaaaaaaaaaa",
+            "answer": False
         },
         {
-            "input":"aA1",
-            "answer":False
+            "input": "aA1",
+            "answer": False
         },
         {
-            "input":"awzbdzkfz",
-            "answer":False
+            "input": "awzbdzkfz",
+            "answer": False
         },
         {
-            "input":"RCAGOSHTTS",
-            "answer":False
+            "input": "RCAGOSHTTS",
+            "answer": False
         },
         {
-            "input":"6691219721",
-            "answer":False
+            "input": "6691219721",
+            "answer": False
         },
         {
-            "input":"PVlppfwrT",
-            "answer":False
+            "input": "PVlppfwrT",
+            "answer": False
         },
         {
-            "input":"45ae5lkgz",
-            "answer":False
+            "input": "45ae5lkgz",
+            "answer": False
         },
         {
-            "input":"1cmuPF1Ycz",
-            "answer":True
+            "input": "1cmuPF1Ycz",
+            "answer": True
         },
         {
-            "input":"Pv4HdnUNb",
-            "answer":False
+            "input": "Pv4HdnUNb",
+            "answer": False
         },
         {
-            "input":"jRNfXg6CdM15SLChALq",
-            "answer":True
+            "input": "jRNfXg6CdM15SLChALq",
+            "answer": True
         },
         {
-            "input":"HZeLrcRR3NU5KprAybp",
-            "answer":True
+            "input": "HZeLrcRR3NU5KprAybp",
+            "answer": True
         },
     ],
     "Extra": [
         {
-            "input":"vcugdgywnl",
-            "answer":False
+            "input": "vcugdgywnl",
+            "answer": False
         },
         {
-            "input":"ZFLTIYYNII",
-            "answer":False
+            "input": "ZFLTIYYNII",
+            "answer": False
         },
         {
-            "input":"9995232403",
-            "answer":False
+            "input": "9995232403",
+            "answer": False
         },
         {
-            "input":"p7ymoqxr8",
-            "answer":False
+            "input": "p7ymoqxr8",
+            "answer": False
         },
         {
-            "input":"qB8TDwXpDp",
-            "answer":True
+            "input": "qB8TDwXpDp",
+            "answer": True
         },
         {
-            "input":"K5uzwrFESgmat6X2VYw",
-            "answer":True
+            "input": "123asasasasa",
+            "answer": False
+        },
+
+        {
+            "input": "K5uzwrFESgmat6X2VYw",
+            "answer": True
         }
     ]
 }

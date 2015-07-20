c9dea46b636ff3c9935222840f8dd87d9fc80a73
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-07-20 09:00:49 +0300

    bool covercode for python

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -5,6 +5,10 @@ from checkio_referee import covercodes, ENV_NAME
 import settings_env
 from tests import TESTS
 
+py_cover = """def cover(f, data):
+    return bool(f(data))
+"""
+
 
 class Referee(RefereeCodeGolf):
     DEFAULT_MAX_CODE_LENGTH = 200
@@ -14,5 +18,8 @@ class Referee(RefereeCodeGolf):
     BASE_POINTS = 10
     TESTS = TESTS
     ENVIRONMENTS = settings_env.ENVIRONMENTS
+    ENV_COVERCODE = {
+        ENV_NAME.PYTHON: py_cover
+    }
 
     DEFAULT_FUNCTION_NAME = "golf"
